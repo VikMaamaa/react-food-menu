fc082b17a45b346c7749fedfc0d6bdcb62bd58d7
vikmaamaa <maamaavictor@yahoo.com>
2021-08-11 18:52:32 +0100

    first

---
+++ src/Categories.js
@@ -0,0 +1,14 @@
+/*eslint-disable*/
+import React from 'react';
+
+const Categories = ({categories,filterItems}) => {
+    return <div className="btn-container"> 
+    {categories.map((category,index)=>{
+      return <button className="filter-btn" type="button" onClick={()=>filterItems(category)} key={index}>
+        {category}
+      </button>
+    })}
+     </div>;
+};
+
+export default Categories;
